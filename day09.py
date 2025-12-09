bc35f3e1d8a5c896e0322d9a7909859bbc51f9ec
Rick Molenaar <rick.molenaar@homeqgo.nl>
2025-12-08 21:35:33 -0800

    Day 09 solved

---
+++ day09.py
@@ -0,0 +1,66 @@
+from copy import deepcopy
+from collections import defaultdict
+
+def parse_input(file = 'day09.txt'):
+    with open(file) as f:
+        s = map(lambda l: l.rstrip(), f.readlines())
+    return list(s)
+
+def parse_example():
+    return parse_input('day09example.txt')
+
+def format_input(inp: list[str]):
+    return [tuple(map(int, line.split(','))) for line in inp]
+
+def solve(inp, part, example):
+    edges = defaultdict(set)
+    if part == 2:
+        for i, (x, y) in enumerate(inp):
+            xp, yp = inp[i-1]
+            if x == xp:
+                for ye in range(min(y, yp) + 1, max(y, yp)):
+                    edges[x].add(ye)
+            else:
+                assert y == yp
+                for xe in range(min(x, xp) + 1, max(x, xp)):
+                    edges[xe].add(y)
+    
+    max_area = 0
+    
+    for i, p1 in enumerate(inp):
+        if part == 2 and not example:
+            print('{0}/{1}'.format(i+1, len(inp)))
+        for p2 in inp[i+1:]:
+            min_x = min(p1[0], p2[0])
+            max_x = max(p1[0], p2[0])
+            min_y = min(p1[1], p2[1])
+            max_y = max(p1[1], p2[1])
+            valid = True
+            if part == 2:
+                for xe in range(min_x + 1, max_x):
+                    for ye in edges[xe]:
+                        if min_y < ye < max_y:
+                            valid = False
+                            break
+                    if not valid:
+                        break
+            if valid:
+                area = (abs(p1[0] - p2[0]) + 1) * (abs(p1[1] - p2[1]) + 1)
+                if area > max_area:
+                    max_area = area
+    return max_area
+    
+
+
+def main():
+    example_input = format_input(parse_example())
+    actual_input = format_input(parse_input())
+    for part in (1, 2):
+        for example in (True, False):
+            inp = deepcopy(example_input if example else actual_input)
+            try:
+                yield solve(inp, part, example)
+            except KeyboardInterrupt:
+                raise
+            except Exception as e:
+                yield e
